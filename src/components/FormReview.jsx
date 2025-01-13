171f828ba2fd28356ef0cbc0adacafe464188db6
Arela-Cristina <instagrandine@gmail.com>
2025-01-13 22:07:54 +0100

    feat: set onChangue event

--- src/components/FormReview.jsx
+++ src/components/FormReview.jsx
@@ -1,4 +1,27 @@
+import { useState } from 'react';
+
+const initialFormData = {
+    nome: '',
+    text: '',
+    vote: '',
+}
+
 function FormReview() {
+
+    const [formData, setFormData] = useState([initialFormData])
+
+    //evento per gestioni dei campi dei form 
+
+    function handleChange(e) {
+        const { name, value } = e.target
+
+        console.log('name', name, 'value', value)
+        setFormData({
+            ...formData,
+            [name]: value,
+        })
+    }
+
     return (
 
         < div className="container border my-6 rounded-lg" >
@@ -13,20 +36,20 @@ function FormReview() {
                 <form className='flex flex-col gap-3'>
                     <p className='form-control'>
                         <label htmlFor="name">Nome</label>
-                        <input type="text" placeholder='Anonymous' name='name' id='name' />
+                        <input type="text" placeholder='Anonymous' name='name' id='name' value={formData.name} onChange={handleChange} />
                     </p>
                     <p className='form-control'>
                         <label htmlFor="text">Recensione</label>
-                        <textarea rows="4" name="text" id="text" placeholder='Scrivi la tua recensione'></textarea>
+                        <textarea rows="4" name="text" id="text" placeholder='Scrivi la tua recensione' value={formData.text} onChange={handleChange}></textarea>
                     </p>
                     <p className='form-control'>
                         <label htmlFor="vote">Voto</label>
-                        <select name="vote" id="vote">
+                        <select name="vote" id="vote" value={formData.vote} onChange={handleChange}>
                             <option value="1">1</option>
-                            <option value="1">2</option>
-                            <option value="1">3</option>
-                            <option value="1">4</option>
-                            <option value="1">5</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
                         </select>
                     </p>
                     <button className='self-end bg-blue-950 hover:bg-blue-800 text-white h-10 rounded-lg px-6'>Invia</button>
