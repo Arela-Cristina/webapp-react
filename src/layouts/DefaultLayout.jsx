2e9cc8903cf0ec79c6918998d0c90cba284ab026
Arela-Cristina <instagrandine@gmail.com>
2025-01-14 16:10:37 +0100

    feat:  condition context functionality

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,14 +1,21 @@
 import { Outlet } from 'react-router-dom';
+import { useContext } from 'react';
+import loaderContext from '../context/LoaderContext';
+import Loader from '../components/Loader';
 import Footer from './Footer';
 import Header from './Header';
 
 function DefaultLayout() {
+
+  const { loading } = useContext(loaderContext);
+
   return <>
     <Header />
     <main className='grow'>
       <Outlet />
     </main>
     <Footer />
+    {loading && <Loader />}
   </>
 }
 
